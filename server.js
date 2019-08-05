6a3f6e022a02de119932fa28cc5dbc8a4475041a
jim9112 <jim9112@gmail.com>
2019-08-04 20:29:10 -0400

    connect database

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+
+// connect to database
+connectDB();
 
 app.get('/', (req, res) => res.json({ msg: 'Welcome to the Social Media API....'}));
 
