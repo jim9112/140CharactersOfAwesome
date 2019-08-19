341e25da19bcb6767a3c18997f515904600d9d73
jim9112 <jim9112@gmail.com>
2019-08-19 10:57:40 -0400

    add post functionality added

--- client/src/context/PostState.js
+++ client/src/context/PostState.js
@@ -3,7 +3,11 @@ import React, { useReducer } from 'react';
 import PostContext from './postContext';
 import postReducer from './postReducer';
 
+import { ADD_POST } from '../types';
+
 const PostState = (props) => {
+
+    // hard coded state for testing purposes
     const initialState = {
         posts: [
             {
@@ -16,14 +20,25 @@ const PostState = (props) => {
                 username: 'bill123',
                 post: 'I have some very different words about my day'
             },
+            {
+                id: 3,
+                username: 'johndoe',
+                post: 'I had quiet the day let me tell you about it'
+            },
         ]
     }
 
     const [state, dispatch] = useReducer(postReducer, initialState );
 
+    // add post to feed
+    const addPost = (post) => {
+        dispatch({ type: ADD_POST, payload: post});
+    }
+
     return(
         <PostContext.Provider value={{
             posts: state.posts,
+            addPost
         }}>
         { props.children}
         </PostContext.Provider>
--- client/src/pages/feed.js
+++ client/src/pages/feed.js
@@ -1,16 +1,45 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import Nav from '../components/layout/Nav';
 import Posts from '../components/posts/Posts';
+import PostContext from '../context/postContext';
 
-const feed = () => {
+const Feed = () => {
+    const postContext = useContext(PostContext);
+
+    const { addPost, posts } = postContext;
+
+    // hard coded in username nd id, remove later <<<<<<<<<<<<<<<<<<<<<<
+    const post = {
+        id: posts.length + 1,
+        username: 'jim9112',
+        post: ''
+    };
+    
+    // resets post input
+    const resetForm = () => { 
+        document.getElementById("feed-form").reset();
+      }
+
+    // updates post variable as the user types
+    const onChange = (e) => {
+        post.post = e.target.value;
+    }
+
+    // submitting the form and clearing it
+    const onSubmit = (e) => {
+        e.preventDefault();
+        addPost(post);
+        resetForm();
+    }
+
     return (
         <div className='container feed-page'>
             <Nav />
             <div className="feed-middle">
                 <h1 className="bottom-border orange">Home</h1>
-                <form action="" className="bottom-border">
-                    <textarea maxLength="140" cols="50" placeholder="Say Something"></textarea>
+                <form action="" className="bottom-border" id='feed-form' onSubmit={ onSubmit }>
+                    <textarea maxLength="140" cols="50" placeholder="Say Something" onChange={ onChange }></textarea>
                     <input className="btn" type="submit" value="Post" />
                 </form>
                 <Posts />
@@ -20,4 +49,4 @@ const feed = () => {
     )
 }
 
-export default feed;
+export default Feed;
