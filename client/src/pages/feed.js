39e3d7af53ead01206963ae441477f348c005562
jim9112 <jim9112@gmail.com>
2019-08-21 20:18:25 -0400

    home route checks for token

--- client/src/pages/feed.js
+++ client/src/pages/feed.js
@@ -1,52 +1,57 @@
-import React, { useContext } from 'react';
+/* eslint-disable react/jsx-filename-extension */
+import React, { useContext, useEffect } from 'react';
 
 import Nav from '../components/layout/Nav';
 import Posts from '../components/posts/Posts';
 import PostContext from '../context/post/postContext';
+import AuthContext from '../context/auth/authContext';
 
 const Feed = () => {
-    const postContext = useContext(PostContext);
+  const postContext = useContext(PostContext);
+  const authContext = useContext(AuthContext);
 
-    const { addPost, posts } = postContext;
+  const { addPost, posts } = postContext;
 
-    // hard coded in username nd id, remove later <<<<<<<<<<<<<<<<<<<<<<
-    const post = {
-        id: posts.length + 1,
-        username: 'jim9112',
-        post: ''
-    };
-    
-    // resets post input
-    const resetForm = () => { 
-        document.getElementById("feed-form").reset();
-      }
+  useEffect(() => {
+    authContext.loadUser();
+  }, []);
+  // hard coded in username nd id, remove later <<<<<<<<<<<<<<<<<<<<<<
+  const post = {
+    id: posts.length + 1,
+    username: 'jim9112',
+    post: '',
+  };
 
-    // updates post variable as the user types
-    const onChange = (e) => {
-        post.post = e.target.value;
-    }
+  // resets post input
+  const resetForm = () => {
+    document.getElementById('feed-form').reset();
+  };
 
-    // submitting the form and clearing it
-    const onSubmit = (e) => {
-        e.preventDefault();
-        addPost(post);
-        resetForm();
-    }
+  // updates post variable as the user types
+  const onChange = (e) => {
+    post.post = e.target.value;
+  };
 
-    return (
-        <div className='container feed-page'>
-            <Nav />
-            <div className="feed-middle">
-                <h1 className="bottom-border orange">Home</h1>
-                <form action="" className="bottom-border" id='feed-form' onSubmit={ onSubmit }>
-                    <textarea maxLength="140" cols="50" placeholder="Say Something" onChange={ onChange }></textarea>
-                    <input className="btn" type="submit" value="Post" />
-                </form>
-                <Posts />
-            </div>
-        </div>
-        
-    )
-}
+  // submitting the form and clearing it
+  const onSubmit = (e) => {
+    e.preventDefault();
+    addPost(post);
+    resetForm();
+  };
+
+  return (
+    <div className='container feed-page'>
+      <Nav />
+      <div className="feed-middle">
+        <h1 className="bottom-border orange">Home</h1>
+        <form action="" className="bottom-border" id="feed-form" onSubmit={onSubmit}>
+          <textarea maxLength="140" cols="50" placeholder="Say Something" onChange={onChange}></textarea>
+          <input className="btn" type="submit" value="Post" />
+        </form>
+        <Posts />
+      </div>
+    </div>
+  );
+};
 
 export default Feed;
