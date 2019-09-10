05c2971ff7436123f111e155b8260410ce8a6a7a
jim9112 <jim9112@gmail.com>
2019-09-10 15:32:50 -0400

    Redo register page with material UI

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -2,10 +2,49 @@
 import React, { useState, useContext, useEffect } from 'react';
 
 
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import AlertContext from '../../context/alert/alertContext';
 import AuthContext from '../../context/auth/authContext';
 
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+
+// Page Styles
+const useStyles = makeStyles(theme => ({
+  '@global': {
+    body: {
+      backgroundColor: theme.palette.common.white,
+    },
+  },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    maxWidth: '500px',
+    margin: 'auto'
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
 const Register = (props) => {
   const alertContext = useContext(AlertContext);
@@ -13,6 +52,8 @@ const Register = (props) => {
 
   const { setAlert } = alertContext;
   const { registerUser, error, clearErrors, isAuthenticated  } = authContext;
+
+  const classes = useStyles();
 
   useEffect(() => {
     if (isAuthenticated) {
@@ -55,40 +96,116 @@ const Register = (props) => {
   };
 
   return (
-    <div>
-      <form className="form-container" onSubmit={onSubmit}>
-        <div className="p-4">
-          <label htmlFor="firstName" className='orange' >First Name</label>
-          <input className="float-right" type="text" name="firstName" value={firstName} onChange={onChange} required />
-        </div>
-        <div className="p-4">
-          <label htmlFor="lastName" className='orange' >Last Name</label>
-          <input className="float-right" type="text" name="lastName" value={lastName} onChange={onChange} required />
-        </div>
-        <div className="p-4">
-          <label htmlFor="userName" className='orange' >Username</label>
-          <input className="float-right" type="text" name="userName" value={userName} onChange={onChange} required />
-        </div>
-        <div className="p-4">
-          <label htmlFor="email" className='orange' >Email</label>
-          <input className="float-right" type="email" name="email" value={email} onChange={onChange} required />
-        </div>
-        <div className="p-4">
-          <label htmlFor="password" className='orange' >Password</label>
-          <input className="float-right" type="password" name="password" value={password} onChange={onChange} required />
-        </div>
-        <div className="p-4">
-          <label htmlFor="password2" className='orange' >Confirm Password</label>
-          <input className="float-right" type="password" name="password2" value={password2} onChange={onChange} required />
-        </div>
-        <div>
-          <input type="submit" value="Login" />
-          <Link to='/login'>
-            <input type="button" value='Cancel' />
-          </Link>
-        </div>
-      </form>
-    </div>
+    <Container component="main" maxwidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <form className={classes.form} onSubmit={onSubmit}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                autoComplete="fname"
+                name="firstName"
+                variant="outlined"
+                required
+                fullWidth
+                id="firstName"
+                label="First Name"
+                autoFocus
+                onChange={onChange}
+                required
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="lastName"
+                label="Last Name"
+                name="lastName"
+                autoComplete="lname"
+                onChange={onChange}
+                required
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="userName"
+                label="User Name"
+                name="userName"
+                autoComplete="username"
+                onChange={onChange}
+                required
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                onChange={onChange}
+                required
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                onChange={onChange}
+                required
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password2"
+                label="Confirm Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                onChange={onChange}
+                required
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >Sign Up</Button>
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Link href="#" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    </Container>
   );
 };
 
