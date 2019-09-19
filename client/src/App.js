7d1fada425780e575cd9aeb97ad295384775337b
jim9112 <jim9112@gmail.com>
2019-09-19 13:48:35 -0400

    install fonts locally

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
+import 'typeface-roboto';
 import Login from './pages/login';
 import Feed from './pages/feed';
 import Register from './components/auth/Register';
