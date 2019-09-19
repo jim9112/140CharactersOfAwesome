a57bb25f3819d94b12c7646f58f350811e80a5a0
jim9112 <jim9112@gmail.com>
2019-09-19 12:12:06 -0400

    test

--- server.js
+++ server.js
@@ -24,8 +24,6 @@ if (process.env.Node_ENV === 'production') {
   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 }
 
-
-
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
