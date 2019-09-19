ae0ab6b7c2ff0e56a360861dd4921cc214ce1af7
jim9112 <jim9112@gmail.com>
2019-09-19 15:08:21 -0400

    change to server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const connectDB = require('./config/db');
 const path = require('path');
+const connectDB = require('./config/db');
 
 const app = express();
 
@@ -22,7 +22,7 @@ if (process.env.Node_ENV === 'production') {
   // set static folder
   app.use(express.static('client/build'));
 
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+  app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')));
 }
 
 const PORT = process.env.PORT || 5000;
