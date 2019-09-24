5b6ccfb10e3bf256e05a2769fd9b67b9569306b2
jim9112 <jim9112@gmail.com>
2019-09-24 17:18:43 -0400

    add pop-up for delete

--- client/src/components/posts/Post.js
+++ client/src/components/posts/Post.js
@@ -4,11 +4,18 @@ import React, { useContext } from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import PropTypes from 'prop-types';
 
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
 import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
+import CardContent from '@material-ui/core/CardContent';
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 import Typography from '@material-ui/core/Typography';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 import AuthContext from '../../context/auth/authContext';
 import PostContext from '../../context/post/postContext';
 
@@ -41,6 +48,28 @@ const useStyles = makeStyles({
       color: 'white',
     },
   },
+  deleteButton: {
+    cursor: 'pointer',
+    float: 'right',
+  },
+  cardWidth: {
+    width: '100%',
+  },
+  dialogButton: {
+    backgroundColor: '#151D26',
+  },
+});
+
+// Color theme for page
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#ED8121',
+    },
+    secondary: {
+      main: '#b09fa5',
+    },
+  },
 });
 
 const Post = ({ post }) => {
@@ -49,26 +78,59 @@ const Post = ({ post }) => {
   const { user } = authContext;
   const { deletePost } = postContext;
 
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   const classes = useStyles();
 
-  const onclick = (e) => {
+  const onclick = () => {
     deletePost(post._id);
+    handleClose();
   };
 
   if (user) {
     return (
-
-      <Card className={user.userName === post.userName ? classes.card2 : classes.card}>
-        <CardContent>
-          <Typography component="h3">
-            {post.userName}
-          </Typography>
-          <Typography component="p">
-            {post.content}
-          </Typography>
-          {user.userName === post.userName && <Button onClick={onclick}>Delete</Button>}
-        </CardContent>
-      </Card>
+      <ThemeProvider theme={theme}>
+        <Card className={user.userName === post.userName ? classes.card2 : classes.card}>
+          <CardContent className={classes.cardWidth}>
+            <Typography component="h3">
+              {post.userName}
+            </Typography>
+            <Typography component="p">
+              {post.content}
+            </Typography>
+            {user.userName === post.userName && <DeleteForeverIcon className={classes.deleteButton} onClick={handleClickOpen} />}
+          </CardContent>
+        </Card>
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">Delete this post??</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              {post.content}
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={onclick} color="primary" className={classes.dialogButton}>
+              Yes
+            </Button>
+            <Button onClick={handleClose} color="primary" className={classes.dialogButton} autoFocus>
+              No
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </ThemeProvider>
     );
   } else {
     return(
